b9f129a6344368f001e7a8142d95de7e31848774
Nikolay <nikolay.grozev@gmail.com>
2018-12-31 09:32:18 +1100

    validate lenght

--- src/models/book.py
+++ src/models/book.py
@@ -3,5 +3,5 @@ from server.instance import server
 
 book = server.api.model('Book', {
     'id': fields.Integer(description='Id'),
-    'title': fields.String(required=True, description='Book title')
+    'title': fields.String(required=True, min_length=1, max_length=200, description='Book title')
 })
